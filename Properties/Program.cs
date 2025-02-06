c643dd4462eba4743eb0094bf9914e4b1970cab1
Ana Galvez <anamariagalvez01@gmail.com>
2025-02-06 04:54:31 -0300

    Agregar archivos de proyecto.

---
+++ Properties/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Properties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    class Empleado
+    {
+        private string nombre;
+        private double salario;
+        public Empleado(string nombre)
+        {
+            this.nombre=nombre;
+        }
+
+        public void SetSalario(double salario)
+        {
+            if (salario < 0)
+            {
+                Console.WriteLine("El salario no puede ser negativo. Se le asignará 0 como salario");
+                salario = 0;
+            }
+            else
+            {
+                this.salario = salario;
+            }
+        }
+        public double GetSalario()
+        {
+            return salario;
+        }
+    }
+}
